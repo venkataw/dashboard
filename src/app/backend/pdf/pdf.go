32daa159e63097a35c53a556edf4c36adbad5d63
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2022-06-24 11:37:52 -0400

    Add pdf pages for node info and pvc info

--- src/app/backend/pdf/pdf.go
+++ src/app/backend/pdf/pdf.go
@@ -26,17 +26,36 @@ type Point struct {
 }
 
 var pointMap = map[string]Point{
-	"title.generated":      {30, 60},
-	"title.namespace":      {30, 80},
-	"poddetail.name":       {60, 33},
-	"poddetail.labels":     {30, 50},
-	"poddetail.taints":     {30, 70},
-	"poddetail.containers": {30, 90},
-	"poddetail.pvc":        {30, 130},
-	"poddetail.nodes":      {30, 150},
-	"poddetail.events":     {30, 170},
-	"podlogs.name":         {60, 33},
-	"podlogs.logs":         {30, 50},
+	"title.generated":               {30, 60},
+	"title.namespace":               {30, 80},
+	"poddetail.name":                {60, 33},
+	"poddetail.labels":              {30, 50},
+	"poddetail.taints":              {30, 70},
+	"poddetail.containers":          {30, 90},
+	"poddetail.pvc":                 {30, 130},
+	"poddetail.nodes":               {30, 150},
+	"poddetail.events":              {30, 170},
+	"podlogs.name":                  {60, 33},
+	"podlogs.logs":                  {30, 50},
+	"pvc.name":                      {60, 33},
+	"pvc.state":                     {30, 50},
+	"pvc.storageclass":              {30, 70},
+	"pvc.volume":                    {30, 90},
+	"pvc.labels":                    {30, 110},
+	"pvc.capacity":                  {30, 130},
+	"pvc.events":                    {30, 150},
+	"node.name":                     {65, 33},
+	"node.labels":                   {30, 50},
+	"node.taints":                   {30, 70},
+	"node.osimage":                  {30, 90},
+	"node.ip":                       {30, 110},
+	"node.schedulable":              {70, 120},
+	"node.state.networkunavailable": {90, 140},
+	"node.state.memorypressure":     {90, 150},
+	"node.state.diskpressure":       {90, 160},
+	"node.state.pidpressure":        {90, 170},
+	"node.state.ready":              {90, 180},
+	"node.events":                   {30, 195},
 }
 
 const reportHeight float64 = 297
@@ -81,22 +100,51 @@ func GenerateTestReport() error {
 	addText(pdf, "poddetail.name", "SAMPLE-POD-ABCDEF1234567890")
 	addText(pdf, "poddetail.labels", "LABEL1, LABEL2, LABEL3, LABEL4, LABEL5")
 	addText(pdf, "poddetail.taints", "SAMPLE.SAMPLE/SAMPLE:SAMPLE op=Exists for 300s")
-	addText(pdf, "poddetail.containers", "CONTAINER1\r\nCONTAINER2\r\nCONTAINER3")
+	addText(pdf, "poddetail.containers", "CONTAINER1", "CONTAINER2", "CONTAINER3")
 	addText(pdf, "poddetail.pvc", "SAMPLE PVC")
 	addText(pdf, "poddetail.nodes", "NODE1, NODE2, NODE3, NODE4, NODE5")
-	addText(pdf, "poddetail.events", "EVENT1\r\nEVENT2\r\nEVENT3")
+	addText(pdf, "poddetail.events", "EVENT1", "EVENT2", "EVENT3")
 
 	pdf.AddPage()
 	podLogsPage := gofpdi.ImportPage(pdf, "templates/pod_logs.pdf", 1, "/MediaBox")
 	gofpdi.UseImportedTemplate(pdf, podLogsPage, 0, 0, reportWidth, reportHeight)
 	addText(pdf, "podlogs.name", "SAMPLE-POD-ABCDEF1234567890")
-	addText(pdf, "podlogs.logs", "LOG1\nLOG2\nLOG3")
+	addText(pdf, "podlogs.logs", "LOG1", "LOG2", "LOG3", "LOG4", "LOG5")
+
+	pdf.AddPage()
+	nodePage := gofpdi.ImportPage(pdf, "templates/node_detail.pdf", 1, "/MediaBox")
+	gofpdi.UseImportedTemplate(pdf, nodePage, 0, 0, reportWidth, reportHeight)
+	addText(pdf, "node.name", "SAMPLE-NODE1")
+	addText(pdf, "node.labels", "LABEL1, LABEL2, LABEL3")
+	addText(pdf, "node.taints", "SAMPLE.SAMPLE/SAMPLE:SAMPLE op=Exists for 300s")
+	addText(pdf, "node.osimage", "Linux Shminux 22.04 LTS")
+	addText(pdf, "node.ip", "123.123.123.123")
+	addText(pdf, "node.schedulable", "Truefalse")
+	addText(pdf, "node.state.networkunavailable", "Truefalse")
+	addText(pdf, "node.state.memorypressure", "Truefalse")
+	addText(pdf, "node.state.diskpressure", "Truefalse")
+	addText(pdf, "node.state.pidpressure", "Truefalse")
+	addText(pdf, "node.state.ready", "Truefalse")
+	addText(pdf, "node.events", "EVENT1", "EVENT2", "EVENT3")
+
+	pdf.AddPage()
+	pvcPage := gofpdi.ImportPage(pdf, "templates/pvc_detail.pdf", 1, "/MediaBox")
+	gofpdi.UseImportedTemplate(pdf, pvcPage, 0, 0, reportWidth, reportHeight)
+	addText(pdf, "pvc.name", "SAMPLE-PVC-1")
+	addText(pdf, "pvc.state", "bound")
+	addText(pdf, "pvc.storageclass", "local-storage")
+	addText(pdf, "pvc.volume", "SAMPLE-PV-1")
+	addText(pdf, "pvc.labels", "LABEL1, LABEL2, LABEL3, LABEL4")
+	addText(pdf, "pvc.capacity", "100Gi")
+	addText(pdf, "pvc.events", "EVENT1", "EVENT2", "EVENT3")
 
 	err := pdf.OutputFileAndClose("/tmp/Report-" + reportGenerated + ".pdf")
 	return err
 }
 
-func addText(pdf *gofpdf.Fpdf, key string, text string) {
+func addText(pdf *gofpdf.Fpdf, key string, text ...string) {
 	location := pointMap[key]
-	pdf.Text(location.x, location.y, text)
+	for i, item := range text {
+		pdf.Text(location.x, location.y+float64(5*i), item)
+	}
 }
