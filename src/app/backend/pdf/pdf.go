0a95d2e13c797dc2a174d24863f86df388b843a0
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2022-06-29 16:24:20 -0400

    Add node info to health check report

--- src/app/backend/pdf/pdf.go
+++ src/app/backend/pdf/pdf.go
@@ -27,9 +27,11 @@ import (
 	metricapi "github.com/kubernetes/dashboard/src/app/backend/integration/metric/api"
 	"github.com/kubernetes/dashboard/src/app/backend/resource/common"
 	"github.com/kubernetes/dashboard/src/app/backend/resource/logs"
+	"github.com/kubernetes/dashboard/src/app/backend/resource/node"
 	"github.com/kubernetes/dashboard/src/app/backend/resource/pod"
 	"github.com/phpdave11/gofpdf"
 	"github.com/phpdave11/gofpdf/contrib/gofpdi"
+	v1 "k8s.io/api/core/v1"
 )
 
 type Point struct {
@@ -94,22 +96,20 @@ func GenerateReport(namespace string) error {
 	titlePageId := importer.ImportPage(pdf, "templates/title_page.pdf", 1, "/MediaBox")
 	podDetailPageId := importer.ImportPage(pdf, "templates/pod_detail.pdf", 1, "/MediaBox")
 	podLogsPageId := importer.ImportPage(pdf, "templates/pod_logs.pdf", 1, "/MediaBox")
-	//nodePageId := importer.ImportPage(pdf, "templates/node_detail.pdf", 1, "/MediaBox")
+	nodePageId := importer.ImportPage(pdf, "templates/node_detail.pdf", 1, "/MediaBox")
 	//pvcPageId := importer.ImportPage(pdf, "templates/pvc_detail.pdf", 1, "/MediaBox")
 
 	addTitlePage(pdf, titlePageId, namespace)
 
+	// pod detail and logs
 	resp, err := getPodDetail(namespace)
 	if err != nil {
 		log.Printf("Error getting pod detail. Skipping. Error: %v", err)
 	} else {
 		for _, pod := range resp.Pods {
 			log.Printf("Pod detail gotten: %v", pod)
-			var labels string = ""
-			for key, value := range pod.ObjectMeta.Labels {
-				labels += key + ":" + value + ", "
-			}
-			labels = labels[0 : len(labels)-2]
+
+			labels := formatLabelString(pod.ObjectMeta.Labels)
 			logDetail, err := getPodLogs(namespace, pod.ObjectMeta.Name)
 			if err != nil {
 				log.Printf("Error getting logs for pod %s in namespace %s. Error: %v", pod.ObjectMeta.Name, namespace, err)
@@ -120,8 +120,29 @@ func GenerateReport(namespace string) error {
 				logArr[i] = string(value.Timestamp) + "---" + value.Content
 			}
 
-			addPodDetailPage(pdf, podDetailPageId, pod.ObjectMeta.Name, labels, "tbi", pod.ContainerImages, "tbi", pod.NodeName, []string{"tbi"}) // TODO: IMPLEMENT TAINT, PVC, EVENTS
+			// TODO: IMPLEMENT TAINT, PVC, EVENTS
+			addPodDetailPage(pdf, podDetailPageId, pod.ObjectMeta.Name, labels, "tbi", pod.ContainerImages, "tbi", pod.NodeName, []string{"tbi"})
 			addPodLogsPage(pdf, podLogsPageId, pod.ObjectMeta.Name, logArr)
+		}
+	}
+
+	nodes, err := getNodeList()
+	if err != nil {
+		log.Printf("Error getting node list. Error %v", err)
+	} else {
+		for _, node := range nodes.Nodes {
+			// get more specific
+			nodeInfo, err := getNodeDetail(node.ObjectMeta.Name)
+			if err != nil {
+				log.Printf("Error getting more specific node detail for %s, skipping. Error: %v", node.ObjectMeta.Name, err)
+			}
+			log.Printf("node detail gotten: %v", nodeInfo)
+			labels := formatLabelString(node.ObjectMeta.Labels)
+			taints := formatTaintString(nodeInfo.Taints)
+			internalIps := formatInternalIpString(nodeInfo.Addresses)
+			events := formatEventListArray(nodeInfo.EventList)
+			// TODO: IMPLEMENT NODE PRESSURE INFO
+			addNodePage(pdf, nodePageId, node.ObjectMeta.Name, labels, taints, nodeInfo.NodeInfo.OSImage, internalIps, !nodeInfo.Unschedulable, false, false, false, false, string(nodeInfo.Ready), events)
 		}
 	}
 
@@ -149,7 +170,7 @@ func GenerateTestReport() error {
 	addPodLogsPage(pdf, podLogsPageId, "SAMPLE-POD-ABCDEF1234567890", []string{"LOG1", "LOG2", "LOG3", "LOG4", "LOG5"})
 
 	addNodePage(pdf, nodePageId, "NODE1", "LABEL1, LABEL2, LABEL3", "SAMPLE.SAMPLE/SAMPLE:SAMPLE op=Exists for 300s", "Linux Shminux 22.04 LTS", "123.123.123.123",
-		false, false, false, false, false, true, []string{"EVENT1", "EVENT2", "EVENT3"})
+		false, false, false, false, false, "True", []string{"EVENT1", "EVENT2", "EVENT3"})
 
 	addPvcPage(pdf, pvcPageId, "SAMPLE-PVC-1", "bound", "local-storage", "SAMPLE-PV-1", "LABEL1, LABEL2, LABEL3", "100Ti", []string{"EVENT1", "EVENT2", "EVENT3"})
 
@@ -178,7 +199,7 @@ func addPodLogsPage(pdf *gofpdf.Fpdf, podLogsPageId int, podName string, logs []
 	addText(pdf, "podlogs.name", podName)
 	addMultilineText(pdf, "podlogs.logs", logs)
 }
-func addNodePage(pdf *gofpdf.Fpdf, nodePageId int, nodeName, labels, taints, osimage, ip string, schedulable, networkunavailable, memorypressure, diskpressure, pidpressure, ready bool, events []string) {
+func addNodePage(pdf *gofpdf.Fpdf, nodePageId int, nodeName, labels, taints, osimage, ip string, schedulable, networkunavailable, memorypressure, diskpressure, pidpressure bool, ready string, events []string) {
 	pdf.AddPage()
 	importer.UseImportedTemplate(pdf, nodePageId, 0, 0, reportWidth, reportHeight)
 	addText(pdf, "node.name", nodeName)
@@ -191,7 +212,7 @@ func addNodePage(pdf *gofpdf.Fpdf, nodePageId int, nodeName, labels, taints, osi
 	addText(pdf, "node.state.memorypressure", strconv.FormatBool(memorypressure))
 	addText(pdf, "node.state.diskpressure", strconv.FormatBool(diskpressure))
 	addText(pdf, "node.state.pidpressure", strconv.FormatBool(pidpressure))
-	addText(pdf, "node.state.ready", strconv.FormatBool(ready))
+	addText(pdf, "node.state.ready", ready)
 	addMultilineText(pdf, "node.events", events)
 }
 func addPvcPage(pdf *gofpdf.Fpdf, pvcPageId int, pvcName, state, storageclass, volume, labels, capacity string, events []string) {
@@ -240,23 +261,65 @@ func getPodDetail(namespace string) (response pod.PodList, err error) {
 func getPodLogs(namespace string, pod string) (response logs.LogDetails, err error) {
 	resp, err := getHttp("log/" + namespace + "/" + pod)
 	if err != nil {
-		log.Printf("Error getting pod detail in namespace %s, error: %v", namespace, err)
+		log.Printf("Error getting pod logs in namespace %s, error: %v", namespace, err)
 		return logs.LogDetails{}, err
 	}
 	bodyBytes, err := parseHtmlToBytes(resp)
 	if err != nil {
-		log.Printf("Error parsing html of pod detail in namespace %s, error: %v", namespace, err)
+		log.Printf("Error parsing html of pod logs in namespace %s, error: %v", namespace, err)
 		return logs.LogDetails{}, err
 	}
 
 	var logDetails logs.LogDetails = logs.LogDetails{}
 	err = json.Unmarshal(bodyBytes, &logDetails)
 	if err != nil {
-		log.Printf("Error parsing json of pod detail in namespace %s, error: %v", namespace, err)
+		log.Printf("Error parsing json of pod logs in namespace %s, error: %v", namespace, err)
 		return logs.LogDetails{}, err
 	}
 
 	return logDetails, nil
+}
+func getNodeList() (nodes node.NodeList, err error) {
+	resp, err := getHttp("node")
+	if err != nil {
+		log.Printf("Error getting node list, error: %v", err)
+		return node.NodeList{}, err
+	}
+	bodyBytes, err := parseHtmlToBytes(resp)
+	if err != nil {
+		log.Printf("Error parsing html of node list, error: %v", err)
+		return node.NodeList{}, err
+	}
+
+	var nodeDetails node.NodeList = node.NodeList{}
+	err = json.Unmarshal(bodyBytes, &nodeDetails)
+	if err != nil {
+		log.Printf("Error parsing json of node list, error: %v", err)
+		return node.NodeList{}, err
+	}
+
+	return nodeDetails, nil
+}
+func getNodeDetail(nodeName string) (nodeDetail node.NodeDetail, err error) {
+	resp, err := getHttp("node/" + nodeName)
+	if err != nil {
+		log.Printf("Error getting node detail for node %s, error: %v", nodeName, err)
+		return node.NodeDetail{}, err
+	}
+	bodyBytes, err := parseHtmlToBytes(resp)
+	if err != nil {
+		log.Printf("Error parsing html of node detail %s, error: %v", nodeName, err)
+		return node.NodeDetail{}, err
+	}
+
+	var nodeDetails node.NodeDetail = node.NodeDetail{}
+	err = json.Unmarshal(bodyBytes, &nodeDetails)
+	if err != nil {
+		log.Printf("Error parsing json of node detail %s, error: %v", nodeName, err)
+		return node.NodeDetail{}, err
+	}
+
+	return nodeDetails, nil
 }
 func getProtocol() string {
 	if Secure {
@@ -277,4 +340,49 @@ func parseHtmlToBytes(response *http.Response) (result []byte, err error) {
 	}
 	bodyBytes := []byte(body)
 	return bodyBytes, nil
+}
+func formatLabelString(labels map[string]string) string {
+	var formatted string = ""
+	if len(labels) == 0 {
+		return "No labels"
+	}
+	for key, value := range labels {
+		formatted += key + ":" + value + ", "
+	}
+	formatted = formatted[0 : len(formatted)-2]
+	return formatted
+}
+func formatTaintString(taints []v1.Taint) string {
+	var formatted string = ""
+	if len(taints) == 0 {
+		return "No taints"
+	}
+	for _, taint := range taints {
+		formatted += taint.Key + ":" + taint.Value + ", "
+	}
+	formatted = formatted[0 : len(formatted)-2]
+	return formatted
+}
+func formatInternalIpString(addresses []v1.NodeAddress) string {
+	var formatted string = ""
+	if len(addresses) == 0 {
+		return "No addresses"
+	}
+	for _, address := range addresses {
+		if address.Type == "InternalIP" {
+			formatted += address.Address + ", "
+		}
+	}
+	formatted = formatted[0 : len(formatted)-2]
+	return formatted
+}
+func formatEventListArray(events common.EventList) []string {
+	eventArr := make([]string, len(events.Events))
+	for i, event := range events.Events {
+		eventArr[i] = event.Message + ", Reason: " + event.Reason // TODO: ADD MORE DETAILS TO EVENT LIST
+	}
+	if len(eventArr) == 0 {
+		return []string{"No events"}
+	}
+	return eventArr
 }
